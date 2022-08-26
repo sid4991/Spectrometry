6638c2101bd4240ebab66e10c8c761620c2aa63e
sid4991 <53467812+sid4991@users.noreply.github.com>
2022-08-26 15:44:29 -0700

    initial commit

---
+++ 04_Spectral_Mixing.R
@@ -0,0 +1,19 @@
+## ---------------------------
+##
+## Script name: Create_Linear_Model
+##
+## Purpose of script:Spectral mixing using linearcombination
+##
+## Author: Siddharth Chaudhary
+##
+## Date Created: 2022-08-26
+##
+## Copyright (c) Siddharth Chaudhary, 2022
+## Email: siddharth.chaudhary@wsu.edu
+##
+## ---------------------------
+##
+## Notes:
+## Using the formula RWC = RWCs*(1-Fr) + RWCr*(Fr)
+##
+## ---------------------------
