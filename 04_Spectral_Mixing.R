a41538c6ecc562df038138f98281503f08121bf2
sid4991 <53467812+sid4991@users.noreply.github.com>
2022-09-12 10:41:08 -0700

    initial

--- 04_Spectral_Mixing.R
+++ 04_Spectral_Mixing.R
@@ -18,9 +18,13 @@
 ##
 ## ---------------------------
 
+conven <- 0.07 ##0-15% residue cover 7% mean
+med <- 0.22   ## 15-30 % residue cover 22% mean
+conser <- 0.65 ## 30-100 % residue cover 65% mean
+
 CropList <- unique(Residue_08_18$Crop)
 SoilList <- unique(Soil_08_18$Crop)
-Temp <- data.frame()
+LinearMix <- data.frame()
 for (i in (CropList)) {
   Cropi_i <- dplyr::filter(Residue_08_18,Crop == i)
   names(Cropi_i)[6] <- "RWC_Residue"
@@ -31,12 +35,12 @@ for (i in (CropList)) {
       names(Soil_j)[6] <- "RWC_Soil"
       TempSoil <- Soil_j
       TempSoilRes <- merge(TempCrop,TempSoil,by = c("Wvl","Scan"),all = T)
-      Temp <- rbind(Temp,TempSoilRes)
+      LinearMix <- rbind(LinearMix,TempSoilRes)
 
   }
 }
 
-Temp$RWC_conven <- Temp$RWC_Soil*(1-0.07) + Temp$RWC_Residue*(0.07)
-Temp$RWC_med <- Temp$RWC_Soil*(1-0.20) + Temp$RWC_Residue*(0.20)
-Temp$RWC_conserv <- Temp$RWC_Soil*(1-0.60) + Temp$RWC_Residue*(0.60)
+LinearMix$RWC_conven <- LinearMix$RWC_Soil*(1-conven) + LinearMix$RWC_Residue*(conven)
+LinearMix$RWC_med <- LinearMix$RWC_Soil*(1-med) + LinearMix$RWC_Residue*(med)
+LinearMix$RWC_conserv <- LinearMix$RWC_Soil*(1-conser) + LinearMix$RWC_Residue*(conser)
 
