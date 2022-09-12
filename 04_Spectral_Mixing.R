e3bf25d4b8dc949a95ff59d21fdf3d9ae5df5b12
sid4991 <53467812+sid4991@users.noreply.github.com>
2022-09-12 10:35:49 -0700

    initial

--- 04_Spectral_Mixing.R
+++ 04_Spectral_Mixing.R
@@ -17,3 +17,26 @@
 ## Using the formula RWC = RWCs*(1-Fr) + RWCr*(Fr)
 ##
 ## ---------------------------
+
+CropList <- unique(Residue_08_18$Crop)
+SoilList <- unique(Soil_08_18$Crop)
+Temp <- data.frame()
+for (i in (CropList)) {
+  Cropi_i <- dplyr::filter(Residue_08_18,Crop == i)
+  names(Cropi_i)[6] <- "RWC_Residue"
+  TempCrop <- Cropi_i
+    for (j in (SoilList)) {
+      TempSoil <- data.frame()
+      Soil_j <- dplyr::filter(Soil_08_18,Crop == j)
+      names(Soil_j)[6] <- "RWC_Soil"
+      TempSoil <- Soil_j
+      TempSoilRes <- merge(TempCrop,TempSoil,by = c("Wvl","Scan"),all = T)
+      Temp <- rbind(Temp,TempSoilRes)
+
+  }
+}
+
+Temp$RWC_conven <- Temp$RWC_Soil*(1-0.07) + Temp$RWC_Residue*(0.07)
+Temp$RWC_med <- Temp$RWC_Soil*(1-0.20) + Temp$RWC_Residue*(0.20)
+Temp$RWC_conserv <- Temp$RWC_Soil*(1-0.60) + Temp$RWC_Residue*(0.60)
+
